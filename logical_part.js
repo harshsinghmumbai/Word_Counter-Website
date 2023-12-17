527bb2b0d888511fd4fc3d7983a6d63f3d5cf16d
Harsh Singh <harshrsingh552005@gmail.com>
2023-12-17 20:32:00 +0530

    asychronous code add

--- logical_part.js
+++ logical_part.js
@@ -1,15 +1,16 @@
 let textbox = document.getElementById("textbox");
 
-textbox.addEventListener("input", function () {
-    let F = this.value;
+textbox.addEventListener("input", async function () {
+    let F =this.value;
     let H = F.length;
     document.getElementById("Char").innerHTML = H;
 
     F = F.trim();
     let G = F.split(" ");
-    console.log(G);
-    let cleanword = G.filter(function (elm) {
-        return elm != "";
+    // console.log(G);
+    let cleanword = await G.filter( async function (elm) {
+        return await elm != "";
     });
+
     document.getElementById("Word").innerHTML = cleanword.length;
 });
